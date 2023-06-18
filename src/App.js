3ac03af30a811844bcfd49721ef42ae768f6858d
Oscar Mångs <oscar.maangs@gmail.com>
2023-06-18 10:25:44 +0200

    removed re-base

--- src/App.js
+++ src/App.js
@@ -5,18 +5,13 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home"
 import LineUp from "./components/LineUp"
 import Schedule from "./components/Schedule"
-import base from "./base";
 
 class App extends React.Component {
   state = {
     lineup: {},
   }
   componentDidMount(){
-    //Sync with firebase
-    this.ref = base.syncState(`lineup`, {
-      context: this,
-      state: "lineup",
-    });
+
   }
   
   render(){
